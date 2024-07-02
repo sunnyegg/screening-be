b7f7b2c0bbf84f46a491b1a8b2069ed3ebc3709f
sunnyegg <adilatresna21@gmail.com>
2024-07-02 18:09:01 +0700

    fix ig

--- models/instagram.js
+++ models/instagram.js
@@ -9,25 +9,27 @@ module.exports = {
       await page.setViewport({ width: 1080, height: 1024 });
       await page.waitForNetworkIdle();
 
-      try {
-        const element = await page.waitForSelector("span ::-p-text( likes)", {
-          timeout: 1000,
+      await page
+        .waitForSelector("span::-p-text( likes)", {
+          timeout: 2000,
+        })
+        .then(async (res) => {
+          const text = await res.evaluate((el) => el.textContent);
+          const convertedText = utils.ConvertTextToNumber(text, "likes");
+
+          output.set(url, convertedText);
+          await page.close();
+        })
+        .catch(async (err) => {
+          if (err.name === "TimeoutError") {
+            await page.waitForSelector("span::-p-text(Liked by)", {
+              timeout: 1000,
+            });
+
+            output.set(url, "less than 100 likes");
+            await page.close();
+          }
         });
-        const text = await element.evaluate((el) => el.textContent);
-        const convertedText = utils.ConvertTextToNumber(text, "likes");
-
-        output.set(url, convertedText);
-        await page.close();
-      } catch (err) {
-        if (err.name === "TimeoutError") {
-          await page.waitForSelector("span ::-p-text(Liked by)", {
-            timeout: 1000,
-          });
-
-          output.set(url, "less than 100 likes");
-        }
-        await page.close();
-      }
     } catch (err) {
       logger("error", err.message);
       output.set(url, "error => " + err.message);
